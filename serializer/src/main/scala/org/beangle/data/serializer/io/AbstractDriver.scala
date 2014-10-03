4270a6243f42c3a887e7d7a53d5b57dc544b50a2
chaostone <duantihua@gmail.com>
2014-10-03 13:07:08 +0800

    based on commons buffered writer

--- serializer/src/main/scala/org/beangle/data/serializer/io/AbstractDriver.scala
+++ serializer/src/main/scala/org/beangle/data/serializer/io/AbstractDriver.scala
@@ -2,10 +2,11 @@ package org.beangle.data.serializer.io
 
 import java.io.OutputStreamWriter
 import java.io.OutputStream
+import org.beangle.commons.io.BufferedWriter
 
 abstract class AbstractDriver(val encoding: String) extends StreamDriver {
 
   override def createWriter(out: OutputStream): StreamWriter = {
-    createWriter(new OutputStreamWriter(out, encoding))
+    createWriter(new BufferedWriter(new OutputStreamWriter(out, encoding)))
   }
 }
