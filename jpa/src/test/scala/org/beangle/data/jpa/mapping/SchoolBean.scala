68555aa86cfe1fe868e9052180e7818bf5e7e1e5
chaostone <duantihua@gmail.com>
2015-01-25 15:56:41 +0800

    rename testbean

--- jpa/src/test/scala/org/beangle/data/jpa/mapping/SchoolBean.scala
+++ jpa/src/test/scala/org/beangle/data/jpa/mapping/SchoolBean.scala
@@ -16,10 +16,10 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Beangle.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.beangle.data.jpa.dao
+package org.beangle.data.jpa.mapping
 
 import org.beangle.data.model.annotation.code
 @code("school")
-class TestDaoBean {
+class SchoolBean {
 
 }
