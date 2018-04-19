fab873cb4a4b6134d6f5cae0c395379e6a9a2622
段体华 <duantihua@gmail.com>
2018-04-19 21:36:24 +0800

    move scala file to src/test/scala

--- model/src/test/scala/org/beangle/data/model/util/Menu.scala
+++ model/src/test/scala/org/beangle/data/model/util/Menu.scala
@@ -1,3 +1,21 @@
+/*
+ * Beangle, Agile Development Scaffold and Toolkits.
+ *
+ * Copyright © 2005, The Beangle Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.beangle.data.model.util
 
 import org.beangle.data.model.pojo.Hierarchical
@@ -63,9 +81,8 @@ class Profile {
     menu.parent = menus.find(x => x.indexno == parentIndexNo)
     menu.parent foreach { p =>
       p.children += menu
-      if(p.id==21) println(p.children)
     }
-    
+
     this.menus += menu
     this
   }
